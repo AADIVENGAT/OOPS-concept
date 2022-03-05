8c8aeb68a9dca689f6ff22f3d5bf658d4afeac20
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-05 07:12:32 +0530

    Add files via upload

---
+++ march4.2.py
@@ -0,0 +1,15 @@
+#OOPS concept:
+class Person:
+    def speak(self):
+        print("I'm speaking...")
+    def walk(self):    
+        print("I'm walking...")
+
+obj_1 = Person()  
+print(type(obj_1))
+obj_2 = Person()        
+obj_3 = Person()        
+obj_4 = Person()   
+
+obj_1.speak()
+obj_2.walk()
